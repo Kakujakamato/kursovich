00521880e9d33ba5eafeee4c1b929ccbef3f6679
tuan <anhtuanhn200022@gmail.com>
2021-11-14 14:33:07 +0300

    init

--- ClientPlayer.py
+++ ClientPlayer.py
@@ -128,11 +128,12 @@ while running:
                         send_data = '{}-{}-{}-{}'.format(cellX, cellY, 'yourturn', playing).encode()
                         sock.send(send_data)
                         turn = False
-            for y in range(len(Broad)):
-                for x in range(len(Broad[1])):
-                    if (Broad[y][x] != 0):
-                        game_over = True
-                        print('Draw\n Press space to continute')
+            else:
+                for y in range(len(Broad)):
+                    for x in range(len(Broad[1])):
+                        if (Broad[y][x] != 0):
+                            game_over = True
+                            print('Draw\n Press space to continute')
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and game_over:
                 restart()
